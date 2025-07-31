38f83bb251212c27c8b61e48c9f30eab1437eb05
Roshan Roy <roshanroy1121@gmail.com>
2025-07-31 19:35:23 +0530

    created login API

--- src/app.js
+++ src/app.js
@@ -1,39 +1,49 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
 const { User } = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
 app.use(express.json()); // middleware to read JSON data
 
 app.post("/signup", async (req, res) => {
-  const userObj = req.body;
-  const user = new User(userObj);
   try {
-    const ALLOWED_FIELDS = [
-      "firstName",
-      "lastName",
-      "emailId",
-      "password",
-      "age",
-      "gender",
-      "photoUrl",
-      "about",
-      "skills",
-    ];
-    const isFieldsAllowed = Object.keys(userObj).every((k) =>
-      ALLOWED_FIELDS.includes(k)
-    );
-    if(!isFieldsAllowed) {
-      throw new Error("Data cannot be saved");
-    }
-    if(userObj?.skills.length > 10) {
-      throw new Error("Skills cannot be more than 10");
-    }
+    // validation of data
+    validateSignUpData(req);
+
+    // encrypting password
+    const { firstName, lastName, emailId, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const userObj = { firstName, lastName, emailId, password: passwordHash };
+    const user = new User(userObj);
+
     await user.save();
     res.send("User saved successfully");
   } catch (err) {
     res.status(400).send("Something went wrong " + err.message);
+  }
+});
+
+// login API
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      res.send("Invalid credentials");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      res.send("Login Successfull");
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (err) {
+    res.status(400).send("Error:" + err.message);
   }
 });
 
---
+++ src/utils/validation.js
@@ -0,0 +1,14 @@
+const validator = require("validator");
+
+const validateSignUpData = (req) => {
+  const { firstName, lastName, emailId, password } = req.body;
+  if (!firstName || !lastName) {
+    throw new Error("Please enter your name");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Enter a strong password");
+  }
+};
+
+module.exports = { validateSignUpData };
